5a53b4b8b26072bb6a29dc71e5d262f05687c8bd
tytydraco <tylernij@gmail.com>
2018-02-21 17:49:42 -0800

    renamed var and modulated local dir

--- modules/debloat.py
+++ modules/debloat.py
@@ -7,7 +7,7 @@ import adb
 _AOSPIFY_LOCAL = '/sdcard/aospify/'
 _SCRIPT_DEBLOAT = './device/debloat.sh'
 _SCRIPT_REBLOAT = './device/rebloat.sh'
-_SCRIPT_DEBLOAT_PKG = './device/debloat_pkg.txt'
+_LIST_DEBLOAT_PKG = './device/debloat_pkg.txt'
 
 def debloat():
 	# remove local dir and recreate for smoother updates
@@ -34,14 +34,14 @@ def rebloat():
 	
 	# push script and list
 	adb.push(os.path.abspath(_SCRIPT_DEBLOAT), _AOSPIFY_LOCAL)
-	adb.push(os.path.abspath(_SCRIPT_DEBLOAT_PKG), _AOSPIFY_LOCAL)
+	adb.push(os.path.abspath(_LIST_DEBLOAT_PKG), _AOSPIFY_LOCAL)
 	
 	# TODO: fix literal insertion of final var @kdrag0n	
 	# give it +x perms
 	adb.shell('chmod +x ' + _AOSPIFY_LOCAL + 'rebloat.sh')
 	
 	# run debloat script
-	adb.shell('cd /sdcard/aospify/; sh ' + _AOSPIFY_LOCAL + 'rebloat.sh')
+	adb.shell('cd ' + _AOSPIFY_LOCAL + '; sh ' + _AOSPIFY_LOCAL + 'rebloat.sh')
 	
 	# clean up
 	adb.rmdir(_AOSPIFY_LOCAL)
