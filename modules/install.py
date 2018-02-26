ee0537895416bf71b991b6bef5259f5eb986a77f
tytydraco <tylernij@gmail.com>
2018-02-25 16:48:44 -0800

    added MusicFX and disabled soundalive.

--- modules/install.py
+++ modules/install.py
@@ -6,9 +6,11 @@ import adb
 APK_CAMERA = 'apk/Camera.apk'
 APK_LAUNCHER = 'apk/Launcher.apk'
 APK_PHONE = 'apk/Phone.apk'
+APK_EQ = 'apk/Equalizer.apk'
 
 # install replacement APKs
 def install():
 	adb.install(APK_CAMERA)
 	adb.install(APK_LAUNCHER)
-	adb.install(APK_PHONE)
+	adb.install(APK_LAUNCHER)
+	adb.install(APK_EQ)
