5013e40478305b5f52524c4dff67a7ea472dfb99
kdrag0n <dragon@khronodragon.com>
2018-03-07 15:20:07 -0800

    Change navbar layout to regular

--- modules/setup.py
+++ modules/setup.py
@@ -1,9 +1,11 @@
 import sys
 from . import adb
 
-COMM_CAM_DOUBLE_TAP = 'settings put system double_tab_launch_component com.google.android.GoogleCamera/com.android.camera.CameraLauncher'
-#COMM_IMMERSIVE_FULL = 'settings put global policy_control "immersive.full=*"'
+CMD_CAM_DOUBLE_PWR = 'settings put system double_tab_launch_component com.google.android.GoogleCamera/com.android.camera.CameraLauncher'
+CMD_NAVBAR_LAYOUT = 'settings put secure sysui_nav_bar "space,back;home;recent,menu_ime"'
+#CMD_IMMERSIVE_FULL = 'settings put global policy_control "immersive.full=*"'
 
 def setup():
-	adb.shell(COMM_CAM_DOUBLE_TAP)
-	#adb.shell(COMM_IMMERSIVE_FULL)
+	adb.shell(CMD_CAM_DOUBLE_PWR)
+        adb.shell(CMD_NAVBAR_LAYOUT)
+	#adb.shell(CMD_IMMERSIVE_FULL)
