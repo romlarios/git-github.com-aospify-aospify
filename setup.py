5eb5e41be44e25412cc0712b0304ed43f8f3b5c2
kdrag0n <dragon@khronodragon.com>
2018-03-06 20:39:43 -0800

    Remove tcl/tk libraries

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from cx_Freeze import setup, Executable
 
 import os.path
 PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 setup(name = "AOSPify" ,
       version = "1.0" ,
