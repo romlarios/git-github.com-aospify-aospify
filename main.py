77af083c6226a7408c96ef124aec0b993ca701a9
kdrag0n <dragon@khronodragon.com>
2018-03-06 20:31:51 -0800

    Use relative modules instead of adding modules to path

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import sys, os
+#!/usr/bin/env python3
 
-# add the modules to the python path to allow imports
-sys.path.insert(0, 'modules')
-
-# import each required module using its filename
-import adb
-import debloat
-import install
-import setup
+import os
+import modules as mod
 
 def splash():
 	print('''
@@ -25,10 +19,10 @@ def main():
 	os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 	splash()
-	adb.adb_check()
-	debloat.debloat()
-	install.install()
-	setup.setup()
+	mod.adb_check()
+	mod.debloat()
+	mod.install()
+	mod.setup()
 
 if __name__ == '__main__':
 	main()
---
+++ modules/__init__.py
@@ -0,0 +1,4 @@
+from .adb import adb_check
+from .debloat import debloat
+from .install import install
+from .setup import setup
