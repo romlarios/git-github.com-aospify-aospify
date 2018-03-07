9ff6863da5f66f08ac2525511ae869a6963878f2
tytydraco <tylernij@gmail.com>
2018-03-06 20:04:03 -0800

    disabled immersive because its not necessary for AOSP

--- modules/setup.py
+++ modules/setup.py
@@ -4,8 +4,8 @@ sys.path.insert(0, 'modules')
 import adb
 
 COMM_CAM_DOUBLE_TAP = 'settings put system double_tab_launch_component com.google.android.GoogleCamera/com.android.camera.CameraLauncher'
-COMM_IMMERSIVE_FULL = 'settings put global policy_control "immersive.full=*"'
+#COMM_IMMERSIVE_FULL = 'settings put global policy_control "immersive.full=*"'
 
 def setup():
 	adb.shell(COMM_CAM_DOUBLE_TAP)
-	adb.shell(COMM_IMMERSIVE_FULL)
+	#adb.shell(COMM_IMMERSIVE_FULL)
