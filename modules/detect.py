6d536567342021228b68423c8375245b4974be63
kdrag0n <dragon@khronodragon.com>
2018-03-07 17:47:23 -0800

    Revamp model check

--- modules/detect.py
+++ modules/detect.py
@@ -1,15 +1,32 @@
+import sys
 from . import adb
 
+DEVICE_UNCHECKED = -1
 DEVICE_UNKNOWN = 0
 DEVICE_EXYNOS = 1
 DEVICE_SNAPDRAGON = 2
 
-def get_model():
+_model = DEVICE_UNCHECKED
+
+def model():
+	global _model
+
+	if _model != DEVICE_UNCHECKED:
+		return _model
+
 	name = adb.shell('getprop ro.product.device').strip('\r\n')
 
 	if name in ['dreamqlte', 'dream2qlte']:
-		return DEVICE_SNAPDRAGON
+		_model = DEVICE_SNAPDRAGON
 	elif name in ['dreamlte', 'dream2lte']:
-		return DEVICE_EXYNOS
+		_model = DEVICE_EXYNOS
 	else:
-		return DEVICE_UNKNOWN
+		_model = DEVICE_UNKNOWN
+		choice = input('''UNSUPPORTED DEVICE! Some or all features will not work, and support will not be provided.
+Do you wish to continue? (y/n) ''')
+
+		if not choice.starts_with('y'):
+			print('Aborting.')
+			sys.exit()
+
+	return _model
