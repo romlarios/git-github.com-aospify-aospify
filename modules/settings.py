5ae2981fdba2ba244f0d7d04773a646775123150
tytydraco <tylernij@gmail.com>
2018-03-10 11:37:34 -0800

    push ringtones and set UI sounds

--- modules/settings.py
+++ modules/settings.py
@@ -1,11 +1,15 @@
 from . import adb
 
+AOSPIFY_MEDIA_DIR = 'assets/AOSPify_Media'
+AOSPIFY_MEDIA_DEVICE_DIR = '/sdcard/AOSPify_Media'
 LIST_DELETE_GLOBAL = 'assets/delete_global.txt'
 LIST_DELETE_SYSTEM = 'assets/delete_system.txt'
 LIST_DELETE_SECURE = 'assets/delete_secure.txt'
 
 def settings():
 	print('[*] Finishing up')
+	
+	adb.push(AOSPIFY_MEDIA_DIR, AOSPIFY_MEDIA_DEVICE_DIR)
 
 	# setup overlays first
 	adb.enable_overlays('com.android.settings.AOSPifyOverlays.AOSPify',
@@ -30,7 +34,16 @@ def settings():
 				online_manual_url='https://aospify.github.io/',
 				flip_font_style=1,
 				dialer_default_application='com.google.android.dialer',
-				sms_default_application='com.google.android.apps.messaging')
+				sms_default_application='com.google.android.apps.messaging',
+				car_dock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Dock.ogg',
+				car_undock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Undock.ogg',
+				desk_dock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Dock.ogg',
+				desk_undock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Undock.ogg',
+				lock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Lock.ogg',
+				low_battery_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/LowBattery.ogg',
+				trusted_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Trusted.ogg',
+				unlock_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/Unlock.ogg',
+				wireless_charging_started_sound=AOSPIFY_MEDIA_DEVICE_DIR + '/ui/WirelessChargingStarted.ogg')
 
 	with open(LIST_DELETE_GLOBAL, 'r') as f:
 		for key in f.readlines():
